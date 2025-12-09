632b4318dbfd223d4024e602995e9a94d207162a
sruhshona2197 <sruhshona2197@gmail.com>
2025-12-09 16:56:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# 8-mashq
+bosh_harflar = [s[0] for s in ["dog", "cat", "mouse"]]
+print(bosh_harflar)
+
+
+# 9-mashq
+musbatlar = [i for i in [-5, 3, -1, 0, 7, -2] if i > 0]
+print(musbatlar)
+
+
+# 10-mashq
+juft_toq = ["juft" if i % 2 == 0 else "toq" for i in range(1, 11)]
+print(juft_toq)
+
+
+# 11-mashq
+ildizlar = [i**0.5 for i in [4, 9, 16, 25]]
+print(ildizlar)
+
+
+# 12-mashq
+tanlangan = [i for i in range(0, 51) if i % 5 == 0]
+print(tanlangan)
+
+
+# 13-mashq
+katta = [s.upper() for s in ["hello", "world", "python"]]
+print(katta)
